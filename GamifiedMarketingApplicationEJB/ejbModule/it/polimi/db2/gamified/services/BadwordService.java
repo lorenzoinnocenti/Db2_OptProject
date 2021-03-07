9326f2979ef8d5a184a1b6d446936905a08f9dad
TheFalco <matteo1.falconi@mail.polimi.it>
2021-03-07 16:44:41 +0100

    Add BadwordService

---
+++ GamifiedMarketingApplicationEJB/ejbModule/it/polimi/db2/gamified/services/BadwordService.java
@@ -0,0 +1,30 @@
+package it.polimi.db2.gamified.services;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import it.polimi.db2.gamified.entities.*;
+
+public class BadwordService {
+	@PersistenceContext(unitName = "GamifiedMarketingApplicationEJB")
+	private EntityManager em;
+	
+	public BadwordService() {
+	}
+	
+	//Controlla se la lista di risposte contiene parolacce, e nel caso ritorna null; altrimenti ritorna la lista di risposte
+	public List<String> checkBadword(List<String> answers) {
+		List<Badword> badwords = em.createNamedQuery("Badword.findAll", Badword.class).getResultList();
+		
+		for (String answer : answers) {
+			for (Badword badword : badwords) {
+				if (answer.contains(badword.getWord())) {
+					return null;
+				}
+			}
+		}
+		return answers;
+	}
+}
