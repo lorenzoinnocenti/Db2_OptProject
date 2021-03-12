d86505546cdcc5650a47b3a572b61d96119d2a0a
Luca-Massini <luca.massini @mail.polimi.it>
2021-03-12 22:31:19 +0100

    fixed the wrong html loading

---
+++ GamifiedMarketingApplicationWEB/src/it/polimi/db2/gamified/controllers/Statistical.java
@@ -0,0 +1,58 @@
+package it.polimi.db2.gamified.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import it.polimi.db2.gamified.entities.Account;
+
+@WebServlet("/Statistic")
+public class Statistical extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
+
+	public Statistical() {
+		super();
+	}
+	
+	
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// If the user is not logged in (not present in session) redirect to the login
+		HttpSession session = request.getSession();
+		Account u = (Account) session.getAttribute("account");
+		if (session.isNew() || u == null) {
+			String loginpath = getServletContext().getContextPath() + "/index.html";
+			response.sendRedirect(loginpath);
+			return;
+		}
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		templateEngine.process("/WEB-INF/Statistical.html", ctx, response.getWriter()); 
+	}
+	
+	
+	public void destroy() {
+	}
+}
+
